db2f1cd3acf4c9645b432ef66c3c679c8d9e27b7
Akash Yadav <136945099+YdvAkash@users.noreply.github.com>
2026-02-25 13:20:57 +0530

    Initial commit - log parsing service

---
+++ src/main/java/com/loganalyzer/common/constants/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.loganalyzer.common.constants;
+
+public class KafkaTopics {
+    public static final String RAW_LOGS = "raw-logs";
+    public static final String ANALYZED_LOGS = "analyzed-logs";
+    public static final String ALERTS = "alerts-topic";
+    public static final String CLUSTERED_LOGS = "clustered-logs";
+}
